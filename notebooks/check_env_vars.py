f79f6210394ec9cb8157af280d05cc018a796dcd
Agnieszka Augustyniak <agnieszka.augustyniak@databricks.com>
2023-03-07 21:35:15 +1100

    Update debug notebook

--- notebooks/check_env_vars.py
+++ notebooks/check_env_vars.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 import os
 
-env_vars = ["DBT_ACCESS_TOKEN", "PAT_TOKEN", "DBT_WAREHOUSE_ID", "DBT_HOST"]
+env_vars = ["DBT_ACCESS_TOKEN", "PAT_TOKEN", "DBT_WAREHOUSE_ID", "DBT_HOST", "'DBT_HOST'", "'DBT_WAREHOUSE_ID'", "'DBT_ACCESS_TOKEN"]
 for env_var in env_vars:
     print(env_var, os.environ.get(env_var))
