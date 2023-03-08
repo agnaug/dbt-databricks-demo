71196f4faf9afb54adc25c2c20161dee0c97448c
Agnieszka Augustyniak <agnieszka.augustyniak@databricks.com>
2023-03-08 16:40:21 +1100

    Update instructions

--- notebooks/check_env_vars.py
+++ notebooks/check_env_vars.py
@@ -1,6 +1,9 @@
 # Databricks notebook source
 import os
 
-env_vars = ["DBT_ACCESS_TOKEN", "PAT_TOKEN", "DBT_WAREHOUSE_ID", "DBT_HOST", "'DBT_HOST'", "'DBT_WAREHOUSE_ID'", "'DBT_ACCESS_TOKEN"]
+env_vars = ["DBT_ACCESS_TOKEN", "PAT_TOKEN", "DBT_WAREHOUSE_ID", "DBT_HOST"]
 for env_var in env_vars:
     print(env_var, os.environ.get(env_var))
+
+# The "DBT_WAREHOUSE_ID", "DBT_HOST", "DBT_ACCESS_TOKEN" get revoked as soon as dbt task finishes
+# However it does not try to overwrite it with any other value
